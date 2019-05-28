9052c4a241b5163519b975dd653e2371d6be2c9d
Cody Brewer <brewer.cody.m@gmail.com>
2019-05-27 22:38:51 -0600

    started database methods

---
+++ databasemethods.js
@@ -0,0 +1,7 @@
+const db = require('../data/dbConfig');
+
+const getDishes = () => {};
+const addDish = dish => {};
+const getDish = id => {};
+const getRecipes = () => {};
+const addRecipe = recipe => {};
