5ac73494ed6b0c0701630bb5dc448281245975b9
Cody Brewer <codybrewer@opt005314.optiv.com>
2019-05-28 15:10:41 -0600

    getRecipes return dish name too

--- api/Models/recipes.js
+++ api/Models/recipes.js
@@ -1,7 +1,7 @@
 db = require('../../data/dbConfig');
 //- `getRecipes()`: should return a list of all recipes in the database including the **dish** they belong to.
 const getRecipes = () => {
-  return db('recipes');
+  return db('recipes').join('dishes', "recipes.dish_id", 'dishes.id').select('recipes.id', 'recipes.name as Recipe_name', 'dishes.name as Dish_Name');
 };
 
 
