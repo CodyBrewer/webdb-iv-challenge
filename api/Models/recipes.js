3466514bd42b50232e536a28ebc2315f12ed3a0f
Cody Brewer <codybrewer@opt005314.optiv.com>
2019-05-28 14:23:35 -0600

    add recipe working

--- api/Models/recipes.js
+++ api/Models/recipes.js
@@ -1,7 +1,13 @@
 db = require('../../data/dbConfig');
-
+//- `getRecipes()`: should return a list of all recipes in the database including the **dish** they belong to.
 const getRecipes = () => {
   return db('recipes');
 };
 
-module.exports = { getRecipes };
+
+// - `addRecipe(recipe)`: should add a **recipe** to the database and return the `id` of the new **recipe**.
+const addRecipe = (recipe) => {
+  return db('recipes').insert(recipe);
+}
+
+module.exports = { getRecipes, addRecipe };
--- api/Routes/recipes.js
+++ api/Routes/recipes.js
@@ -5,7 +5,7 @@ const db = require('../Models/recipes');
 const recipesRouter = express.Router();
 
 const { validateRecipesId } = require('../../Middleware');
-const { getRecipes } = db;
+const { getRecipes, addRecipe } = db;
 recipesRouter.use((req, res, next) => {
   console.log('recipesRouter is working');
   next();
@@ -21,5 +21,15 @@ recipesRouter.get('/', async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+recipesRouter.post('/', async (req, res) => {
+  try{
+    const recipe = await addRecipe(req.body);
+    res.status(201).json(recipe);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message});
+  }
+})
 
 module.exports = recipesRouter;
