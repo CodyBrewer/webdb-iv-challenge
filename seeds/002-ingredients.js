cd7dd0c84d354f3acbee892db6001f889bd80ab1
Cody Brewer <brewer.cody.m@gmail.com>
2019-05-28 21:07:44 -0600

    renaming unit of measurement column to unit

--- seeds/002-ingredients.js
+++ seeds/002-ingredients.js
@@ -8,242 +8,241 @@ exports.seed = function(knex, Promise) {
         {
           name: 'pizza crust mix',
           quantity: 2,
-          'unit of measurement': 'packages (6-1/2 ounces)',
+          unit: 'packages (6-1/2 ounces)',
           recipe_id: 1
         },
         {
           name: 'olive oil',
           quantity: 1,
-          'unit of measurement': 'tablespoon',
+          unit: 'tablespoon',
           recipe_id: 1
         },
         {
           name: 'garlic powder',
           quantity: 1.5,
-          'unit of measurement': 'teaspoons',
+          unit: 'teaspoons',
           recipe_id: 1
         },
         {
           name: 'ground beef',
           quantity: 0.25,
-          'unit of measurement': 'pound',
+          unit: 'pound',
           recipe_id: 1
         },
         {
           name: 'onion powder',
           quantity: 0.5,
-          'unit of measurement': 'teaspoon',
+          unit: 'teaspoon',
           recipe_id: 1
         },
         {
           name: 'spaghetti sauce',
           quantity: 0.66,
-          'unit of measurement': 'cup',
+          unit: 'cup',
           recipe_id: 1
         },
         {
           name: 'sliced pepperoni',
           quantity: 1,
-          'unit of measurement': 'packages (3-1/2 ounces)',
+          unit: 'packages (3-1/2 ounces)',
           recipe_id: 1
         },
         {
           name: 'Canadian Bacon',
           quantity: 6,
-          'unit of measurement': 'ounces',
+          unit: 'ounces',
           recipe_id: 1
         },
         {
           name: 'sliced fresh mushrooms',
           quantity: 0.25,
-          'unit of measurement': 'cup',
+          unit: 'cup',
           recipe_id: 1
         },
         {
           name: 'sliced ripe olives',
           quantity: 2,
-          'unit of measurement': 'tablesoons',
+          unit: 'tablesoons',
           recipe_id: 1
         },
         {
           name: 'shredded part-skim mozzarella cheese',
           quantity: 2,
-          'unit of measurement': 'cups',
+          unit: 'cups',
           recipe_id: 1
         },
         {
           name: 'Goya Tomato Sauce',
           quantity: 2,
-          'unit of measurement': '8 ounce can',
+          unit: '8 ounce can',
           recipe_id: 2
         },
         {
           name: 'Goya White Distilled Vinegar',
           quantity: 2,
-          'unit of measurement': 'teaspoons',
+          unit: 'teaspoons',
           recipe_id: 2
         },
         {
           name: 'Goya Minced Garlic',
           quantity: 1,
-          'unit of measurement': 'teaspoons',
+          unit: 'teaspoons',
           recipe_id: 2
         },
         {
           name: 'ancho chile powder',
           quantity: 3.5,
-          'unit of measurement': '',
+          unit: '',
           recipe_id: 2
         },
         {
           name: 'Goya Ground Cumin',
           quantity: 1,
-          'unit of measurement': 'teaspoon',
+          unit: 'teaspoon',
           recipe_id: 2
         },
         {
           name: 'Goya Oregano Leaf',
           quantity: 1,
-          'unit of measurement': 'teaspoon',
+          unit: 'teaspoon',
           recipe_id: 2
         },
         {
           name: 'sugar',
           quantity: 1,
-          'unit of measurement': 'teaspoon',
+          unit: 'teaspoon',
           recipe_id: 2
         },
         {
           name: 'Goya Extra Virgin Olive Oil',
           quantity: 2,
-          'unit of measurement': 'tablespoons',
+          unit: 'tablespoons',
           recipe_id: 2
         },
         {
           name: 'Bone-in, skin-on chicken breasts',
           quantity: 2,
-          'unit of measurement': 'pounds',
+          unit: 'pounds',
           recipe_id: 2
         },
         {
           name: 'Goya Pepper ',
           quantity: 1,
-          'unit of measurement': 'tablespoons',
+          unit: 'tablespoons',
           recipe_id: 2
         },
         {
           name: 'Goya Corn Tortillas',
           quantity: 1,
-          'unit of measurement':
-            '(10 Ounce) package Goya Corn Tortillas, warmed',
+          unit: '(10 Ounce) package Goya Corn Tortillas, warmed',
           recipe_id: 2
         },
         {
           name: 'Finely Chopped White Onion',
           quantity: 0.25,
-          'unit of measurement': 'cup',
+          unit: 'cup',
           recipe_id: 2
         },
         {
           name: 'lime, cut into wedges',
           quantity: 1,
-          'unit of measurement': 'lime',
+          unit: 'lime',
           recipe_id: 2
         },
         {
           name: 'Coarsely Chpped Fresh Cilantro',
           quantity: 2,
-          'unit of measurement': 'tablespoons',
+          unit: 'tablespoons',
           recipe_id: 2
         },
         {
           name: 'Goya Hot Sauce',
           quantity: 1,
-          'unit of measurement': '(12 Ounce) Bottle',
+          unit: '(12 Ounce) Bottle',
           recipe_id: 2
         },
         {
           name: 'bacon, smoked, not frozen',
           quantity: 0.5,
-          'unit of measurement': 'pounds',
+          unit: 'pounds',
           recipe_id: 3
         },
         {
           name: 'bacon, frozen slightly',
           quantity: 1,
-          'unit of measurement': 'pounds',
+          unit: 'pounds',
           recipe_id: 3
         },
         {
           name: 'ground beef 90% or better',
           quantity: 2,
-          'unit of measurement': 'pounds',
+          unit: 'pounds',
           recipe_id: 3
         },
         {
           name: 'garlic powder or granulated garlic',
           quantity: 1.5,
-          'unit of measurement': 'teaspoons',
+          unit: 'teaspoons',
           recipe_id: 3
         },
         {
           name: 'grated onion',
           quantity: 4,
-          'unit of measurement': 'tablespoons',
+          unit: 'tablespoons',
           recipe_id: 3
         },
         {
           name: 'freshly ground black pepper',
           quantity: 0.5,
-          'unit of measurement': 'teaspoons',
+          unit: 'teaspoons',
           recipe_id: 3
         },
         {
           name: 'Worcestershire sauce, optional',
           quantity: 2,
-          'unit of measurement': 'tablespoons',
+          unit: 'tablespoons',
           recipe_id: 3
         },
         {
           name: 'Cheddar, Gouda or American (an orange cheese)',
           quantity: 12,
-          'unit of measurement': 'slices',
+          unit: 'slices',
           recipe_id: 3
         },
         {
           name: 'Swiss, Provolone or Jack cheese (a white cheese)',
           quantity: 12,
-          'unit of measurement': 'slices',
+          unit: 'slices',
           recipe_id: 3
         },
         {
           name: 'red onion, slice',
           quantity: 1,
-          'unit of measurement': 'red onion',
+          unit: 'red onion',
           recipe_id: 3
         },
         {
           name: 'tomato, slice',
           quantity: 1,
-          'unit of measurement': 'tomato',
+          unit: 'tomato',
           recipe_id: 3
         },
         {
           name: 'Green Lettuce',
           quantity: 1,
-          'unit of measurement': 'head',
+          unit: 'head',
           recipe_id: 3
         },
         {
           name: 'Brioche Buns',
           quantity: 1,
-          'unit of measurement': 'package of 12',
+          unit: 'package of 12',
           recipe_id: 3
         },
         {
           name: 'Butter',
           quantity: 1,
-          'unit of measurement': 'stick',
+          unit: 'stick',
           recipe_id: 3
         }
       ]);
