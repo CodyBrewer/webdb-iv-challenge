039ba6b6331b8512b6d9b1cfa56b7c56945c9a28
Cody Brewer <brewer.cody.m@gmail.com>
2019-05-29 20:13:42 -0600

    instruction model created

---
+++ models/instructions.js
@@ -0,0 +1,27 @@
+const db = require('../data/dbConfig');
+
+const getInstructions = () => {
+  return db('instructions');
+};
+
+const getInstruction = id => {
+  return db('instructions')
+    .where({ id })
+    .first();
+};
+
+const addInstruction = instruction => {
+  return db('instructions').insert(instruction);
+};
+
+const deleteInstruction = id => {
+  return db('instructions')
+    .where({ id })
+    .delete();
+};
+
+const updateInstruction = (id, changes) => {
+  return db('instructions')
+    .where({ id })
+    .update(changes);
+};
